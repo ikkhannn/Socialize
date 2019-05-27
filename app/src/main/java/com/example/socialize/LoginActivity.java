a2593f2a1d45e4d990f9abcd7f58729030b867b9
ikkhannn <imrankhannn81@gmail.com>
2019-05-27 18:29:48 +0500

    Added some changes.

--- app/src/main/java/com/example/socialize/LoginActivity.java
+++ app/src/main/java/com/example/socialize/LoginActivity.java
@@ -23,7 +23,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
-
+EditText a;
     EditText email,password;
     Button login;
     TextView txt_signup;
